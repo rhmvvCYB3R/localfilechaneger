2bd59d704e9739ca6b6f8e3dfc9d980b35b8bdc8
rhmvvCYB3R <10ragimov10@gmail.com>
2025-07-13 14:03:45 +0200

    ДОБАВЛЕНЫ ПРОСТО ФАЙЛЫ

---
+++ backend/FileChanger/src/main/java/com/filechanger/FileChanger/FileChangerApplication.java
@@ -0,0 +1,13 @@
+package com.filechanger.FileChanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileChangerApplication.class, args);
+	}
+
+}
---
+++ backend/FileChanger/src/test/java/com/filechanger/FileChanger/FileChangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filechanger.FileChanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileChangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
